1b98fe89311ab546d31ed20457244bf0d2f1fd28
Gustavo Caqui <gustavocaqui94@gmail.com>
2023-06-03 22:41:06 -0500

    agregando rutas principales

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,11 +7,21 @@ import {connectFirestoreEmulator, getFirestore, provideFirestore} from "@angular
 import {environment} from "../environments/environment";
 import {connectAuthEmulator, getAuth, provideAuth} from "@angular/fire/auth";
 
+export const firebaseConfig = {
+    apiKey: "AIzaSyDkoOMhxux5vPznOtgVTT7bXl-Y3uN2Z9E",
+    authDomain: "auth-firebase-angular-e54c1.firebaseapp.com",
+    projectId: "auth-firebase-angular-e54c1",
+    storageBucket: "auth-firebase-angular-e54c1.appspot.com",
+    messagingSenderId: "139005490875",
+    appId: "1:139005490875:web:da7238e9a09c2d925cdb9e"
+}
+
+
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
         importProvidersFrom(
-            provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+            provideFirebaseApp(() => initializeApp(firebaseConfig)),
             provideAuth( () => {
                 const auth = getAuth();
                 connectAuthEmulator(auth, 'http://localhost/9099',{disableWarnings: true})
